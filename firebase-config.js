9caf70252b26c0ddb1fc8e5670eaf81df8fbfde1
J105588 <jxjin2010@gmail.com>
2025-11-22 10:33:32 +0900

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnqE4g0Cs2ZUBgq4bX2_W9Jrl4vGXT3UA",
+  authDomain: "concil-hub.firebaseapp.com",
+  projectId: "concil-hub",
+  storageBucket: "concil-hub.firebasestorage.app",
+  messagingSenderId: "611994338432",
+  appId: "1:611994338432:web:1f785adba95b16c29cd265"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
